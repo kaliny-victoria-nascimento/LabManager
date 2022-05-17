76ff7117383e48aa2d211266e398482553d56407
Kaliny Victória <kalinyvic05@gmail.com>
2022-05-16 23:13:19 -0300

    Adicionando os comandos para Lab List e Lab New

--- Program.cs
+++ Program.cs
@@ -61,3 +61,60 @@ if(modelName == "Computer")
         connection.Close();
     }
 }
+
+connection.Open();
+
+command.CommandText = @"
+    CREATE TABLE IF NOT EXISTS Lab(
+        id int not null primary key,
+        number int not null,
+        name varchar(100) not null,
+        block int not null
+    );
+";
+
+command.ExecuteNonQuery();
+connection.Close();
+
+if(modelName == "Lab")
+{
+    if(modelAction == "List")
+    {
+        Console.WriteLine("Lab List");
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Lab";
+
+        var reader = command.ExecuteReader();
+        
+        while(reader.Read())
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetInt32(3));
+        }
+        
+        connection.Close();
+    }
+
+    if(modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        string name = args[4];
+        var block = args[5];
+
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Lab VALUES($id, $number, $name, $block)";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
